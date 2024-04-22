bbad38169276f061c42106c49972837d98823094
arsal <arsal.h@yahoo.com>
2024-04-22 20:28:59 +0500

    vite and vuejs and vuex4 is installed, setting up store

---
+++ backend/src/store/index.js
@@ -0,0 +1,9 @@
+const { createStore } = require("vuex/types/index.js");
+
+const store = createStore({
+    // creating objects
+    state: {},
+    getter: {},
+    actions: {},
+    mutations: {},
+});
