5e21c33a4b65deed81647af9d5a383a7a7dfd39b
arsal <arsal.h@yahoo.com>
2024-04-23 21:29:32 +0500

    GuestLayout with props and submit action completed

--- backend/src/views/Signup.vue
+++ backend/src/views/Signup.vue
@@ -1,9 +1,91 @@
 <template>
-  <h2>Signup</h2>
+  <GuestLayout title="Sign Up" @submit="signup">
+    <div>
+      <label for="text" class="block text-sm font-medium leading-6 text-gray-900"
+        >Name</label
+      >
+      <div class="mt-2">
+        <input
+          id="name"
+          name="name"
+          type="text"
+          autocomplete=""
+          required=""
+          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </div>
+    <div>
+      <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
+        >Email address</label
+      >
+      <div class="mt-2">
+        <input
+          id="email"
+          name="email"
+          type="email"
+          autocomplete="email"
+          required=""
+          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </div>
+
+    <div>
+      <div class="flex items-center justify-between">
+        <label for="password" class="block text-sm font-medium leading-6 text-gray-900"
+          >Password</label
+        >
+      </div>
+      <div class="mt-2">
+        <input
+          id="password"
+          name="password"
+          type="password"
+          autocomplete="current-password"
+          required=""
+          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+      <div class="flex items-center justify-between">
+        <label for="password" class="block text-sm font-medium leading-6 text-gray-900"
+          >Confirm Password</label
+        >
+      </div>
+      <div class="mt-2">
+        <input
+          id="password"
+          name="password"
+          type="password"
+          autocomplete="current-password"
+          required=""
+          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+      <div class="text-sm">
+        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500"
+          >Already have an account?</a
+        >
+      </div>
+    </div>
+
+    <div>
+      <button
+        type="submit"
+        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Sign in
+      </button>
+    </div>
+  </GuestLayout>
 </template>
 
 <script setup>
+import GuestLayout from "../components/GuestLayout.vue";
 name: "Signup";
+function signup() {
+  console.log("sign-up");
+}
 </script>
 
 <style scoped></style>
